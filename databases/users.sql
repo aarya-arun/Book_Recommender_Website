0b1eb3ed8d1e62981d31248af35756b97ffd5bfb
Aarya Arun <45157845+aarya-arun@users.noreply.github.com>
2020-02-24 17:29:39 +0530

    Create users.sql

---
+++ databases/users.sql
@@ -0,0 +1,70 @@
+CREATE TABLE user(
+
+											username varchar(250),
+											email_id varchar(250),
+											password varchar(40),
+											shelf_id int,
+											avg_rating int,
+											
+											
+											
+											
+									);
+									
+
+CREATE TABLE tenfavbooks (
+
+														username varchar(250),
+														isbn int,
+														
+														
+													);
+													
+													
+CREATE TABLE top5genres (
+
+														username varchar (250),
+														genre_id int
+                            
+                         );
+                         
+                         
+CREATE TABLE top5authors (
+
+														username varchar (250),
+														author_id int
+                            
+                         );
+                         
+ 
+ CREATE TABLE friends(
+ 
+                  username varchar(250),
+                  friend varchar(250),
+                  similarity_index float,
+                  
+                  
+                  );
+                  
+                 
+									
+
+
+
+
+
+
+											Users:
+Username
+Password
+Avg rating
+Shelves
+Top 10 fav books
+Top 5 genres --
+Top 5 favourite authors---
+Friends---
+Reviews---
+
+											
+											
+		
