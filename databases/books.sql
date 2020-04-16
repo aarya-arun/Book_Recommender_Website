f74bc9798b043a5f7faf186a5c9cec7ccbb9ae07
Aarya Arun <45157845+aarya-arun@users.noreply.github.com>
2020-04-16 20:34:10 +0530

    Update books.sql

--- databases/books.sql
+++ databases/books.sql
@@ -1,16 +1,19 @@
 CREATE TABLE books(
-											TBD
-											
-											
-											
-										);
-										
-										
-										
-CREATE TABLE book_reviews(
 	
-	isbn int,
-	username varchar(250),
-	review varchar (5000)
+  isbn int,
+  authors varchar(200),
+  original_publication_year int,
+  original_title varchar (500),
+  language_code varchar(5),
+  average_rating float,
+  ratings_count int,
+  text_reviews_count int,
+  image_url varchar(500),
+  small_image_url varchar(500),
+  authorsmatch varchar(200),
+  titlesmatch varchar(500),
+  book_desc varchar(5000),
+  genres varchar(50),
+  genresmatch varchar(50)
 	
-	);
+);
