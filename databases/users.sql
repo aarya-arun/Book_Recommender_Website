b61851b0474bf702ebe74a65c042bd383323c148
Aarya Arun <45157845+aarya-arun@users.noreply.github.com>
2020-05-28 16:05:54 +0530

    Update users.sql

--- databases/users.sql
+++ databases/users.sql
@@ -1,64 +1,22 @@
-CREATE TABLE users(
-
-											username varchar(250),
-											email_id varchar(250),
-											password varchar(40),
-											age int,
-											shelf_id int,
-											
-											
-											primary key (username),
-                                                                                        unique(email_id),
-                                                                                        unique(shelf_id)
-											
-											
-									);
+CREATE TABLE users(username varchar(250),email_id varchar(250),password1 varchar(40),age int,shelf_id int,primary key (username),unique(email_id), unique(shelf_id));
 									
 									
-CREATE TABLE user_ratings(
-	username varchar(250),
-	isbn int,
-	rating int
-	);
+CREATE TABLE user_ratings(username varchar(250),isbn int,rating int );
 									
 
-CREATE TABLE topfifteenbooks (
-
-		username varchar(250),
-		isbn int,
-														
-														
-		);
+CREATE TABLE topfifteenbooks (username varchar(250),isbn int);
 													
 													
-CREATE TABLE topfivegenres (
-
-			username varchar (250),
-			genre varchar(30)
-                            
-                         );
+CREATE TABLE topfivegenres (username varchar (250),genre varchar(30));
                          
  
- CREATE TABLE friends(
- 
-                  username varchar(250),
-                  friend varchar(250),
-                  similarity_index float,
-                  
-                  
-                  );
+ CREATE TABLE friends( username varchar(250),friend varchar(250), similarity_index float);
 		  
 		  
-CREATE TABLE have_read (
-	username varchar(250),
-	isbn int
-	);
+CREATE TABLE have_read (username varchar(250),isbn int);
 	
 	
-CREATE TABLE want_to_read (
-	username varchar(250),
-	isbn int
-	);
+CREATE TABLE want_to_read (username varchar(250),isbn int);
                   
                  
 									
@@ -68,23 +26,21 @@ alter table topfifteenbooks add constraint crossref1 foreign key (isbn) referenc
 
 alter table topfifteenbooks add constraint crossref2 foreign key (username) references users(username) on delete cascade on update cascade;
 
-alter table topfivegenres add constraint crossref3 foreign key(username) references users(username) on delete cascade on update cascade;
+alter table topfivegenres add constraint crossref3 foreign key (username) references users(username) on delete cascade on update cascade;
 
-alter table topfivegenres add constraint crossref4 foreign key (username) references users(username) on delete cascade on update cascade;
+alter table friends add constraint crossref4 foreign key(username) references users(username) on delete cascade on update cascade;
 
-alter table friends add constraint crossref5 foreign key(username) references users(username) on delete cascade on update cascade;
+alter table friends add constraint crossref5 foreign key(friend) references users(username) on delete cascade on update cascade;
 
-alter table friends add constraint crossref6 foreign key(friend) references users(username) on delete cascade on update cascade;
+alter table user_ratings add constraint crossref6 foreign key (isbn) references books(isbn) on delete cascade on update cascade;
 
-alter table user_ratings add constraint crossref7 foreign key (isbn) references books(isbn) on delete cascade on update cascade;
-
-alter table user_ratings add constraint crossref8 foreign key(username) references users(username) on delete cascade on update cascade;
+alter table user_ratings add constraint crossref7 foreign key(username) references users(username) on delete cascade on update cascade;
 											
-alter table want_to_read add constraint crossref9 foreign key (isbn) references books(isbn) on delete cascade on update cascade;
+alter table want_to_read add constraint crossref8 foreign key (isbn) references books(isbn) on delete cascade on update cascade;
 
-alter table want_to_read add constraint crossref10 foreign key(username) references users(username) on delete cascade on update cascade;
+alter table want_to_read add constraint crossref9 foreign key(username) references users(username) on delete cascade on update cascade;
 																					
-alter table have_read add constraint crossref11 foreign key (isbn) references books(isbn) on delete cascade on update cascade;
+alter table have_read add constraint crossref10 foreign key (isbn) references books(isbn) on delete cascade on update cascade;
 
-alter table have_red add constraint crossref12 foreign key(username) references users(username) on delete cascade on update cascade;
+alter table have_read add constraint crossref11 foreign key(username) references users(username) on delete cascade on update cascade;
 													
